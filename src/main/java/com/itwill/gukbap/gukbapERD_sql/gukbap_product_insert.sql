006f7c3b584d9970dd086ce9979284987c8b7f20
mimimi89 <jejek89@gmail.com>
2020-10-21 17:33:56 +0800

    제품, 제품 카테고리 insert

---
+++ src/main/java/com/itwill/gukbap/gukbapERD_sql/gukbap_product_insert.sql
@@ -0,0 +1,63 @@
+
+--제품 추가(제품 번호 빼고 null 허용)
+--국밥: 굴국밥, 돼지국밥, 소고기국밥, 순대국밥, 콩나물국밥
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '굴국밥', 8000, product_image, '**********싱싱한 통영산 굴이 가득 들어있는 영양만점 국밥**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '돼지국밥', 8000, product_image, '**********돼지다리뼈를 넣고 우려낸 뽀얀국물에 돼지고기와 양념장이 올라가는 고기국밥**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '소고기국밥', 8500, product_image, '**********소고기에 콩나물과 우거지를 넣고 푹 끓인 얼큰한 국밥**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '순대국밥', 8000, product_image, '**********토종순대와 시래기를 넣고 끓인 푸짐한 양의 순대국밥**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '콩나물국밥', 7000, product_image, '**********무공해로 재배되는 콩나물에 20여가지 천연재료를 사용하여 우려낸 콩나물국밥**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+--사이드: 굴전, 모듬순대, 소고기수육
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '굴전', 14000, product_image, '**********굴 본연의 맛과 향을 느낄 수 있는 굴전**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '모듬순대', 20000, product_image, '**********머릿고기와 세 가지 종류의 순대를 한 접시에**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '소고기수육', 38000, product_image, '**********소고기 사태와 양지를 황태육수에 삶아 부추와 함께 즐기는 소고기수육**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+--콜라, 사이다, 생수, 참이슬후레쉬, 카스병맥주
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '코카콜라', 3000, product_image, '**********1.5L 큰 페트병(일회용컵 제공 안함)**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '코카콜라', 2000, product_image, '**********355ml 뚱뚱한 캔(빨대 제공 안함)**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '스프라이트', 3000, product_image, '**********1.5리터 큰 페트병(일회용컵 제공 안함)**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '스프라이트', 2000, product_image, '**********355ml 뚱뚱한 캔(빨대 제공 안함)**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '참이슬후레쉬', 4000, product_image, '**********참이슬후레쉬(일회용컵 제공 안함)**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '처음처럼', 4000, product_image, '**********처음처럼(일회용컵 제공 안함)**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '테라병맥주', 5000, product_image, '**********테라//병맥주(일회용컵 제공 안함)**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+INSERT INTO product (product_no, product_name, product_price, product_image, product_desc, product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date) 
+VALUES (product_product_no_SEQ, '카스병맥주', 5000, product_image, '**********카스//병맥주(일회용컵 제공 안함)**********', product_sale_count, product_click_count, category_no, product_order, product_isOnSale, product_discountRate, product_sale_date);
+
+commit;
+
+
+
+
+
+
+
+
+
