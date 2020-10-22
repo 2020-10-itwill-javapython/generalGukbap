58a505c06554250b2ef17c8e8e95c821e9eb8e6a
DESKTOP-90KH7RV\jaeil <jaeil759@gmail.com>
2020-10-22 22:51:51 +0900

    product추가

---
+++ src/main/java/com/itwill/gukbap/repository/ProductRepositoryImpl.java
@@ -0,0 +1,59 @@
+package com.itwill.gukbap.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.itwill.gukbap.domain.ProductDomain;
+import com.itwill.gukbap.mapper.ProductMapper;
+
+
+@Repository
+public class ProductRepositoryImpl implements ProductRepository {
+	@Autowired
+	ProductMapper productMapper;
+	
+	@Override
+	public int insertProduct(ProductDomain product) {
+		return productMapper.insertProduct(product);
+	}
+	
+	@Override
+	public List<ProductDomain> selectAll() {
+		return productMapper.selectAll();
+	}
+	
+	@Override
+	public ProductDomain selectProductByProductName(String product_name) {
+		return productMapper.selectProductByProductName(product_name);
+	}
+	
+	@Override
+	public ProductDomain selectProductByProductNo(int product_no) {
+		return productMapper.selectProductByProductNo(product_no);
+	}
+	
+//	@Override
+//	public List<ProductDomain> selectProductByCategoryNo(int category_no) {
+//		return productMapper.selectProductByCategoryNo(category_no);
+//	}
+	
+	@Override
+	public int updateProduct(ProductDomain product) {
+		return productMapper.updateProduct(product);
+	}
+	
+	@Override
+	public int deleteProductByProductName(String product_name) {
+		return productMapper.deleteProductByProductName(product_name);
+	}
+	
+	@Override
+	public int deleteProductByProductNo(int product_no) {
+		return productMapper.deleteProductByProductNo(product_no);
+	}
+
+
+
+}
