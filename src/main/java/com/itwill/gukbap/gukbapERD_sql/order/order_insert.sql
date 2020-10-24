3d33a408987b95db7fdaa763ddd5cb37fb792d97
DESKTOP-90KH7RV\jaeil <jaeil759@gmail.com>
2020-10-25 02:29:43 +0900

    오더 생각하다 열받아서 삭제

---
+++ src/main/java/com/itwill/gukbap/gukbapERD_sql/order/order_insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO gukbap_order (order_no, order_date, order_status, order_total_pricce, order_desc, order_note, user_id) 
+VALUES (order_no, order_date, order_status, order_total_pricce, order_desc, order_note, user_id);
