d97df6f85d6c6f15b6c640bab446dbdcc215269e
hyunwoo <qnqkd23@naver.com>
2020-10-29 17:21:27 +0900

    controller

---
+++ src/main/java/com/itwill/gukbap/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.itwill.gukbap.controller;
+
+import org.springframework.stereotype.Controller;
+
+import com.itwill.gukbap.service.UserService;
+
+@Controller
+public class UserController {
+	
+	UserService userService;
+	
+	
+
+}
