278b3bd6b7102e5ff63fc75a52c5e9eaf55c7b1b
mimimi89 <jejek89@gmail.com>
2020-11-15 16:16:05 +0900

    테스트 컨트롤러 --> 국밥 컨트롤러

--- src/main/java/com/itwill/gukbap/controller/TestController.java
+++ src/main/java/com/itwill/gukbap/controller/TestController.java
@@ -93,10 +93,6 @@ public class TestController {
                 
 		return "cart";
 	}
-	
-	
-	
-	
 	@RequestMapping("f_product_list")  
 	public String f_product_list(@RequestParam int c_no,HttpServletRequest request) {
 		List<ProductDomain> productList=productService.selectProductByCategoryNo(c_no);
@@ -104,12 +100,6 @@ public class TestController {
 		request.setAttribute("productList",productList);
 		return "f_product_list";
 	}
-	/*
-	@RequestMapping(value = "add_to_cart",method = RequestMethod.GET)
-	private String get_product_GET(HttpServletRequest request,HttpServletResponse response,@RequestParam String product_no)throws Exception {
-		return "redirect:/cart";
-	}
-	*/
 	
 	@RequestMapping(value = "add_to_cart",method = RequestMethod.POST)
 	private String add_to_cart(HttpServletRequest request,@RequestParam String product_no,@RequestParam String pty) {
@@ -125,62 +115,6 @@ public class TestController {
 		return "cart";
 	}
 	
-	@RequestMapping(value = "main_to_cart",method = RequestMethod.POST)
-	private String main_to_cart(HttpServletRequest request, @RequestParam String product_no, @RequestParam String pty) {
-		ProductDomain product=productService.selectProductByProductNo(Integer.parseInt(product_no));
-		System.out.println(pty);
-		System.out.println(product_no);
-		//UserDomain user = (UserDomain) request.getSession().getAttribute("loginUser");
-		//user.getUser_id()
-//		orderService.insertOrder(
-//		"big-test@naver.com", 
-//		new OrderDetailDomain(0, 20, 1, productService.selectProductByProductNo(2)));
-		orderService.insertOrder("jaeil@naver.com",new OrderDetailDomain(0,0,Integer.parseInt(pty),product));
-		return "cart";
-	}
-	
-	@RequestMapping(value = "wishlist_to_cart",method = RequestMethod.POST)
-	private String wishlist_to_cart(@RequestParam String product_no, @RequestParam String pty, HttpServletRequest request) {
-		ProductDomain product=productService.selectProductByProductNo(Integer.parseInt(product_no));
-		
-		//UserDomain user = (UserDomain) request.getSession().getAttribute("loginUser");
-		//String user_id=user.getUser_id();	
-		orderService.insertOrder("jaeil@naver.com", new OrderDetailDomain(0,0,1,product));
-		return "wishlist";
-	}
-	
-	@RequestMapping("gukbap_main")  
-	public String index_product_list(HttpServletRequest request) {
-		List<ProductDomain> indexProductList=productService.selectProductByCategoryNo(1);
-		request.setAttribute("indexProductList", indexProductList);
-		List<ProductDomain> indexCountList=productService.selectProductOrderByClickCount();
-		request.setAttribute("indexCountList", indexCountList);
-		List<ReviewDomain> indexReviewList=reviewService.selectAllReviewArrangeInTheLatestFive();
-		request.setAttribute("indexReviewList", indexReviewList);
-		return "gukbap_main";
-	}
-	
-	@RequestMapping("f_wishlist")  
-	public String f_wishlist(@RequestParam String wishlist_no,HttpServletRequest request) {
-		wishListService.removeItemFromWishList(Integer.parseInt(wishlist_no));
-		//UserDomain user = (UserDomain) request.getSession().getAttribute("loginUser");
-		//user.getUser_id()
-		//request.setAttribute("productList",productList);
-		List<WishListDomain> wishlist=wishListService.getWishListItems("jaeil@naver.com");
-		request.setAttribute("wishlist",wishlist);
-		return "f_wishlist";
-	}
-	
-	@RequestMapping(value="wishlist", method=RequestMethod.GET)
-	public String show_wishlist(HttpServletRequest request) {
-		//UserDomain user = (UserDomain) request.getSession().getAttribute("loginUser");
-		//user.getUser_id()
-		//wishlistService.getWishListItems(user_id);
-		List<WishListDomain> wishlist=wishListService.getWishListItems("jaeil@naver.com");
-		request.setAttribute("wishlist", wishlist);
-		return "wishlist";
-	}
-	
 	@RequestMapping(value = "add_wishlist",method = RequestMethod.POST)
 	private String add_wishlist(@RequestParam String product_no,HttpServletRequest request) {
 		//UserDomain user = (UserDomain) request.getSession().getAttribute("loginUser");
@@ -188,17 +122,6 @@ public class TestController {
 		wishListService.addToWishList("jaeil@naver.com",Integer.parseInt(product_no));
 		return "wishlist";
 	}
-
-	@RequestMapping(value = "main_to_wishlist",method = RequestMethod.POST)
-	private String main_to_wishlist(@RequestParam String product_no,HttpServletRequest request) {
-		//UserDomain user = (UserDomain) request.getSession().getAttribute("loginUser");
-		//String user_id=user.getUser_id();	
-		wishListService.addToWishList("jaeil@naver.com",Integer.parseInt(product_no));
-		return "wishlist";
-	}
-	
-
-
 	/********************************************************************/
 	
 	@RequestMapping("guest_list")
