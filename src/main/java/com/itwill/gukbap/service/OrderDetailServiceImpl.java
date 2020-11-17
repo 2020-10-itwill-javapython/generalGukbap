cc09b324a2d156dfaada6f7dd822dc20fb5e9733
helprun <akiratksk@hotmail.com>
2020-11-17 16:04:51 +0900

    카트 다 수정한듯... 진짜루..제발...그렇다고 해줘...

--- src/main/java/com/itwill/gukbap/service/OrderDetailServiceImpl.java
+++ src/main/java/com/itwill/gukbap/service/OrderDetailServiceImpl.java
@@ -41,24 +41,37 @@ public class OrderDetailServiceImpl implements OrderDetailService {
 				orderRepository.selectOrderByNo(order_no);
 		List<OrderDetailDomain> orderItems = order.getOrderDetailList();
 		
-		
 		int order_total_price = 0;
-		for (OrderDetailDomain orderDetailDomain : orderItems) {
-			order_total_price += 
-					orderDetailDomain.getProduct().getProduct_price() * 
-					orderDetailDomain.getO_d_product_count();
+		if (orderItems.size() == 1 && orderItems.get(0).getProduct() == null) {
+			orderRepository.
+			updateOrder(
+					new OrderDomain(
+							order.getOrder_no(), 
+							"sysdate", 
+							"preparing", 
+							order_total_price, 
+							"", 
+							"", 
+							order.getUser_id(), null));
+		} else {
+			for (OrderDetailDomain orderDetailDomain : orderItems) {
+				order_total_price += 
+						orderDetailDomain.getProduct().getProduct_price() * 
+						orderDetailDomain.getO_d_product_count();
+			}
+			
+			orderRepository.
+			updateOrder(
+					new OrderDomain(
+							order.getOrder_no(), 
+							"sysdate", 
+							"preparing", 
+							order_total_price, 
+							orderItems.get(0).getProduct().getProduct_name() + "외 "+ (orderItems.size() - 1) +"개 상품", 
+							"", 
+							order.getUser_id(), null));
 		}
 		
-		orderRepository.
-		updateOrder(
-				new OrderDomain(
-						order.getOrder_no(), 
-						"sysdate", 
-						"preparing", 
-						order_total_price, 
-						orderItems.get(0).getProduct().getProduct_name() + "외 "+ (orderItems.size() - 1) +"개 상품", 
-						"", 
-						order.getUser_id(), null));
 		
 	}
 }
